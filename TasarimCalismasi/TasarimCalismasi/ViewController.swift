2554e3818fb436a949c1e8f0c3bb1284b65ccfcb
Berat Paban <67541132+beratpaban@users.noreply.github.com>
2024-11-24 20:08:16 +0300

    3.gun tamamlandi

---
+++ TasarimCalismasi/TasarimCalismasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasarimCalismasi
+//
+//  Created by Berat Paban on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
